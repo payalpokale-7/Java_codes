45f7bc7bb8678e3864fcdd68e02b44ec002a68f5
payal pokale <78795031+payalpokale-7@users.noreply.github.com>
2023-04-25 14:15:34 +0530

    reflection

--- Demo1.java
+++ Demo1.java
@@ -1,49 +1,41 @@
-package wrapperandstring;
-
+package reflection;
+import java.lang.reflect.*;
+class RDemo
+{
+	public int x;
+	final int y=20;
+	public RDemo() {
+		
+	}
+	public RDemo(int x) {
+		
+	}
+	public void show() {
+		
+	}
+	public void display(int e)
+	{
+		System.out.println("In display()"+e);
+	}
+}
 public class Demo1 {
 
-	public static void main(String[] args) {
-		int x=52;
-		//Autoboxing
-		Integer i1=new Integer(23);
-		Integer i2=new Integer(x);
-		System.out.println(i1+" "+i2);
-		//using method
-		Integer i3=Integer.valueOf(78);
-		Double d1=Double.valueOf(52.31);
-		System.out.println(i3);
-		System.out.println(d1);
-		//Unboxing
-		int x1=i1.intValue();
-		int x2=i3.intValue();
-		double d=d1.doubleValue();
-		System.out.println(x1);
-		System.out.println(x2);
-		System.out.println(d);
-		
-		//String into Wrapper class object
-		Integer i4=new Integer("10");
-		Double d2=new Double("56.67");
-		Float f1=new Float("5.2f");
-		System.out.println(i4);
-		System.out.println(d2);
-		System.out.println(f1);
-		
-		//String into primitive datatype
-		int a2=Integer.parseInt("12");
-		float f2=Float.parseFloat("2.3");
-		System.out.println("Sum="+(a2+a2));
-		
-		//Wrapper class object into String
-		String s1=i4.toString();
-		String s2=d2.toString();
-		System.out.println(s1);
-		System.out.println(s2);
-		
-		//primitive datatype into String
-		String s3=Integer.toString(10);
-		System.out.println("Sum="+(s3+s3));
-	
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	RDemo r=new RDemo();
+	Class c=r.getClass();
+	System.out.println(c.getName());
+	Method m[]=c.getMethods();
+	for(Method i:m) {
+		System.out.println(i);
+	}
+	Constructor con[]=c.getConstructors();
+	for(Constructor i:con)
+	{
+		System.out.println(i);
+	}
+	Method m1=c.getDeclaredMethod("display", int.class);
+	m1.invoke(r, 25);
+
 	}
 
 }
